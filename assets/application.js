c0f789c936197f7ba3d6f1bef65f092348073b03
josimar <josimardec@gmail.com>
2022-08-26 19:25:38 -0300

    eigth

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,9 @@
 // Put your application javascript here
-document.querySelector('#sort_by').addEventListener('change', function(e) {
-    let url = new URL(window.location.href)
-    url.searchParams.set('sort_by', e.currentTarget.value)
+if (document.getElementById('sort_by') != null) {
+    document.querySelector('#sort_by').addEventListener('change', function(e) {
+        let url = new URL(window.location.href)
+        url.searchParams.set('sort_by', e.currentTarget.value)
 
-    window.location = url.href
-}); 
+        window.location = url.href
+    }); 
+}
