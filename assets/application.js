89dd6f6927c6dedfa2d1324a935b982636db7d53
josimar <josimardec@gmail.com>
2022-09-08 12:59:19 -0300

    twelve

--- assets/application.js
+++ assets/application.js
@@ -7,3 +7,25 @@ if (document.getElementById('sort_by') != null) {
         window.location = url.href
     }); 
 }
+
+if (document.getElementById('AddressCountryNew') != null) {
+    document.getElementById('AddressCountryNew').addEventListener('change', function(e) {
+        let provinces = this.options[this.selectedIndex].getAttribute('data-provinces');
+        let provinceSelector = document.getElementById('AddressProvinceNew');
+        let provinceArray = JSON.parse(provinces);
+
+        if (provinceArray.length < 1) {
+            provinceSelector.setAttribute('disabled', 'disabled')
+        } else {
+            provinceSelector.removeAttribute('disabled')
+        }
+
+        provinceSelector.innerHTML = '';
+        let options = '';
+        for (let i=0; i < provinceArray.length; i++) {
+            options += '<option value="' + provinceArray[i][0] + '">' + provinceArray[i][0] + '</option>'
+        }
+
+        provinceSelector.innerHTML = options;
+    });
+} 
